33089c06ed4869f9515c11cfb2dd25f26c693f4f
Sebastian C <sacasadevall@gmail.com>
2024-12-03 12:35:42 -0500

    Made Thought model, will come back to fix errors

---
+++ src/models/Thought.ts
@@ -0,0 +1,79 @@
+import { Schema, model, Document, Types } from 'mongoose';
+
+export interface IReaction {
+    reactionId: Types.ObjectId;
+    reactionBody: string;
+    username: string;
+    createdAt: Date;
+}
+
+export interface IThought extends Document {
+    thoughtText: string;
+    createdAt: Date;
+    username: string;
+    reactions: IReaction[];
+    reactionCount: number;
+}
+
+const reactionSchema = new Schema<IReaction>(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp: Date) => timestamp.toLocaleString(),
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+const thoughtSchema = new Schema<IThought>(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp: Date) => timestamp.toLocaleString(),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+export const Thought = model<IThought>('Thought', thoughtSchema);
